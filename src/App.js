141d6a394a4c4ca21e632abb2faf1621690e6cc9
kishanmalhotra65 <kishanmalhotra65@gmail.com>
2023-05-09 22:26:53 +0530

    header started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1>Hello thinkdigital</h1>
+    <div classNameName="App">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{"backgroundColor": "#FFFFFF"}}>
+        <div className="container-fluid">
+          <div className='row'>
+             <div className='col-3'>
+               
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="/">Montra App</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Platforms</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Solutions</a>
+                  </li>
+                  </ul> 
+              
+              </div>
+               <div className='col-6'></div>
+               <div className='col-3'>
+         
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Nigeria
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="/">Action</a></li>
+                      <li><a class="dropdown-item" href="/">Another action</a></li>
+                      <li><a class="dropdown-item" href="/">Something else here</a></li>
+                    </ul>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link active" href="/">Company </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Resourses</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Help</a>
+                  </li>
+                  </ul> 
+              
+          </div>
+        </div>
+        </div>
+      </nav>
+      
     </div>
   );
 }
