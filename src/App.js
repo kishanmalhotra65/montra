71c7a4d9bdb69ff3a9e43ae7e4fed583e867a045
shambhu_bcoder <shambhunath@bcoder.in>
2023-05-11 22:25:43 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
+import Footer from "./components/Footer/Footer.jsx"
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
