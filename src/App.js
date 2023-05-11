2c249494101e1505414ca8e42f9f9d7a60bddfae
shambhu_bcoder <shambhunath@bcoder.in>
2023-05-11 21:34:34 +0530

    Header setup code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 function App() {
   return (
     <div classNameName="App">
-      <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{"backgroundColor": "#FFFFFF"}}>
-        <div className="container-fluid">
-          <div className='row'>
-             <div className='col-3'>
-               
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <a className="nav-link active" href="/">Montra App</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="/">Platforms</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="/">Solutions</a>
-                  </li>
-                  </ul> 
-              
-              </div>
-               <div className='col-6'></div>
-               <div className='col-3'>
-         
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Nigeria
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="/">Action</a></li>
-                      <li><a class="dropdown-item" href="/">Another action</a></li>
-                      <li><a class="dropdown-item" href="/">Something else here</a></li>
-                    </ul>
-                  </li>
-
-                  <li className="nav-item">
-                    <a className="nav-link active" href="/">Company </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="/">Resourses</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="/">Help</a>
-                  </li>
-                  </ul> 
-              
-          </div>
-        </div>
-        </div>
-      </nav>
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/paymentSection/paymentSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function paymentSection() {
+    return (
+        <div>paymentSection</div>
+    )
+}
+
+export default paymentSection
