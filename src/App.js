cac4e8f33e42c07051db620e191d94ff86d83f48
shambhu_bcoder <shambhunath@bcoder.in>
2023-05-13 18:15:55 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import Footer from "./components/Footer/Footer.jsx";
 import PaymentSection from './components/PaymentSection/PaymentSection';
+import Bank from "./components/Bank/Bank"
 function App() {
   return (
     <div className="App">
       <Header />
       <PaymentSection />
+      <Bank />
       <Footer />
+
     </div>
   );
 }
--- src/components/Bank/Bank.js
+++ src/components/Bank/Bank.js
@@ -3,7 +3,19 @@ import bankingImage from "../../assets/banking.png"
 function Bank() {
     return (
         <div className='banking'>
+            <div className='container-fluid'>
+                <div className='row'>
 
+                    <div className='col-6'></div>
+                    <div className='col-6'>
+                        <img src={bankingImage} alt="" srcset="" />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-6'></div>
+                    <div className='col-6'></div>
+                </div>
+            </div>
         </div>
     )
 }
