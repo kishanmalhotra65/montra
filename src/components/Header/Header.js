7e23c7a032ee06f421dbfefce21cda5536c17dd0
kishanmalhotra65 <kishanmalhotra65@gmail.com>
2023-05-17 00:24:59 +0530

    code updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,7 @@ function Header() {
     const [country, setCountry] = useState("India");
     const [flag, setFlag] = useState("https://flagcdn.com/in.svg");
 
-    // const getCountryFlag = async (name) => {
-    //     let result = await countriesJSON.find((country) => country?.name.toLowerCase() === name?.toLowerCase());
-    //     return result.flag
-    // }
+   
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch("https://ipapi.co/json/");
